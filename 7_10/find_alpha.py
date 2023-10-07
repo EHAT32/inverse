ca2c7ba227549e6379b64158bc719fde6f9e8d33
Roman <enatik14@gmail.com>
2023-10-07 19:14:59 +0300

    4th lab

--- 7_10/find_alpha.py
+++ 7_10/find_alpha.py
@@ -3,7 +3,7 @@ import torch
 
 def fourier_solve(kernel_fft, right_fft, m_omega, alpha):
     sol_fft = (right_fft * kernel_fft.conj()) / (kernel_fft.conj() * kernel_fft + alpha * m_omega)
-    return np.fft.ifft(sol_fft).real
+    return torch.fft.ifft(sol_fft).real
 
 def conv(kernel, func):
     result = np.zeros(len(func))
@@ -35,50 +35,46 @@ def loss_2(kernel : torch.Tensor, kernel_fft : torch.Tensor, right_fft : torch.T
 # t_ax = np.linspace(a,b,N)
 
 # x_t = lambda t : np.cos(2 * np.pi * f_0 * t + mu * t**2)
+# x_ar = x_t(t_ax)
 
-# beta = 3e3
+# beta_0 = 3e3
 # A = 0.5
 # dt = 2e-3
 
-# kernel = lambda t : np.exp(- beta * t) + A * np.exp(- beta * t - dt)
-# f_t = conv(kernel, x_t, t_ax)
+# kernel = lambda t, beta : np.exp(- beta * t) + A * np.exp(- beta * t - dt)
+# kernel_10 = kernel(t_ax, beta_0 * 0.9)
+# f_10 = conv(kernel_10, x_ar)
+# sigma = 7e-2
+# noise = np.random.normal(0, sigma)
 
-# #Фурье от правой части:
-# kernel_ar = kernel(t_ax)
-# kernel_fft = np.fft.fft(kernel_ar)
+# # #Фурье от правой части:
+# kernel_fft = np.fft.fft(kernel_10)
 # m_omega = lambda omega: omega ** 2 + 1e-4
 # freq_ax = np.fft.fftfreq(len(t_ax), t_ax[1] - t_ax[0])
 # m_ar = m_omega(freq_ax)
 
-def conv(kernel, func, t_ax):
-    result = np.zeros(len(t_ax))
-    K = kernel(t_ax)
-    F = func(t_ax)[::-1]
-    for i in range(len(t_ax)):
-        result[i] = np.sum(K * np.roll(F, i + 1))
-    return result
-
-# kernel_ar = torch.from_numpy(kernel_ar)
+# kernel_ar = torch.from_numpy(kernel_10)
 # kernel_fft = torch.from_numpy(kernel_fft)
 # m_ar = torch.from_numpy(m_ar)
+# target = torch.from_numpy(x_ar)
 
 # epoch_num = 1000
 # alpha_opt = np.zeros(epoch_num)
 
 # for j in range(epoch_num):
-#     sig = 7e-2
 #     alpha = torch.tensor(1e-9, dtype=torch.float32, requires_grad=True)
 #     optimizer = torch.optim.Adam([alpha], lr=1e-11)
-#     noise = np.random.normal(0, sig, len(f_t))
-#     f_d = f_t + noise
+#     noise = np.random.normal(0, sigma, len(f_10))
+#     f_d = f_10 + noise
 #     F_d = torch.from_numpy(np.fft.fft(f_d))
-#     sigma = torch.tensor(sig * 0.8, dtype=torch.float32)
+#     sigma = torch.tensor(sigma, dtype=torch.float32)
 #     for i in range(50):
 #         loss = loss_2(kernel_ar, kernel_fft, F_d, m_ar, alpha, sigma)
+#         # loss = loss_func(kernel_fft, F_d, m_ar, alpha, target)
 #         optimizer.zero_grad()
 #         loss.backward()
 #         optimizer.step()
 #     if j % 100 == 0:
 #         print(f'{j + 1} : {loss}')
 #     alpha_opt[j] = alpha.detach().numpy()
-# np.save('D:/python/inverse/30_9/sec_meth_7_e_2_low.npy', alpha_opt)
+# np.save('C:/python/inverse/7_10/mls_beta_0_9.npy', alpha_opt)
